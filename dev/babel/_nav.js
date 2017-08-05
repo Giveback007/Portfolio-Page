cfb3318034ce749d4086746157a18576fa5092a3
Dovy <dovy91@gmail.com>
2017-08-05 03:11:20 -0500

    Nav bar is now 'sticky' and smooth scroll is working now

--- dev/babel/_nav.js
+++ dev/babel/_nav.js
@@ -1,3 +1,4 @@
+/* Toggle Responsive Nav Start */
 var navIsOn = false;
 function toggleNav() {
   navIsOn = !navIsOn;
@@ -18,3 +19,32 @@ function toggleNav() {
 }
 
 document.getElementById('nav_toggle').addEventListener("click", toggleNav)
+/* Toggle Responsive Nav End */
+
+/* Smooth Scroll Start */
+$(document).ready(function() {
+  var navHeight = $('nav').outerHeight();
+ $('.nav_anchor').click(function(x) {
+    var linkHref = $(this).attr('href');
+    x.preventDefault();
+    $('html, body').delay(150).animate({
+     scrollTop: $(linkHref).offset().top - navHeight
+    }, 1200);
+ });
+});
+/* Smooth Scroll End */
+
+/* Nav Sticky Start */
+$(function () {
+  $(document).on('scroll', function () {
+    if ($(window).scrollTop() >= $("#hero").height()) {
+      $("#nav").addClass("nav_fixed-on");
+      $("#nav_ghost").addClass("nav_ghost");
+    }
+    if ($(window).scrollTop() < $("#hero").height()) {
+      $("#nav").removeClass("nav_fixed-on");
+     $("#nav_ghost").removeClass("nav_ghost");
+    }
+  });
+});
+/* Nav Sticky End */
