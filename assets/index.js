f4daf4cbe9db2dd84d2ba12289d8b8bbd368f3d0
Dovy <dovy91@gmail.com>
2017-08-06 13:38:27 -0500

    implementing arr of objs for portfolio section

--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,54 @@
 'use strict';
 
+//@prepros-append babel/_portfolio-arr.js
 //@prepros-append babel/_nav.js
 //@prepros-append babel/_form.js
+
+var portf = [
+// react.js
+{ name: 'React.js',
+  items: [{ name: 'Game Of Life', img: 'assets/imgs/portfolio/under_construction.jpg', site: 'https://giveback007.github.io/game-of-life-react-js', source: 'https://github.com/Giveback007/game-of-life-react-js' }, { name: 'Leaderboard', img: 'camper-leaderboard.png', site: 'Camper-Leaderboard', source: 'Camper-Leaderboard' }, { name: 'Recipe Box', img: 'recipe-box.png', site: 'Recipe-Box', source: 'Recipe-Box' }, { name: 'Markdown Prev', img: '', site: 'Markdown-Previewer', source: 'Markdown-Previewer' }]
+}];
+
+//,
+// // backend
+// { name: 'Backend',
+//   items: [
+//     {name: '', img: '', site: '', source: ''}
+//   ]
+// },
+// { name: 'JSON/AJAX api',
+//   items: [
+//     {name: '', img: '', site: '', source: ''},
+//     {name: '', img: '', site: '', source: ''},
+//     {name: '', img: '', site: '', source: ''},
+//     {name: '', img: '', site: '', source: ''}
+//   ]
+// },
+// // algorithm
+// { name: 'Algorithm Scripting',
+//   items: [
+//     {name: '', img: '', site: '', source: ''},
+//     {name: '', img: '', site: '', source: ''},
+//     {name: '', img: '', site: '', source: ''}
+//   ]
+// },
+// // cms
+// { name: 'CMS',
+//   items: [
+//     {name: '', img: '', site: '', source: ''},
+//     {name: '', img: '', site: '', source: ''}
+//   ]
+// },
+// // misc
+// { name: 'Misc.',
+//   items: [
+//     {name: '', img: '', site: '', source: ''},
+//     {name: '', img: '', site: '', source: ''},
+//     {name: '', img: '', site: '', source: ''}
+//   ]
+// }
+// ];
 
 /* Toggle Responsive Nav Start */
 var navIsOn = false;
--- dev/babel.js
+++ dev/babel.js
@@ -1,2 +1,3 @@
+//@prepros-append babel/_portfolio-arr.js
 //@prepros-append babel/_nav.js
 //@prepros-append babel/_form.js
